d307765ccb5c94c3c0109ad703fef305953b588a
Vardan Tuladhar <tuladharvr@vcu.edu>
2022-10-05 16:32:48 -0400

    we now have the ability to scan in template values

--- project1/OS_sim.cpp
+++ project1/OS_sim.cpp
@@ -2,14 +2,25 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <vector>
 using namespace std;
 OS_sim :: OS_sim(string template1)
 {
-	string myText; 
-	ifstream temp(template1);
-	while (getline( temp, myText)){
-	cout << myText<< endl;
+	vector <string> Op_names;
+        vector <int> maxcycles;
+	vector <int> mincycles;
+	int mincycle, maxcycle;
+	string Op_name;
+	string firstline;	
+	ifstream temp(template1, ios:: in);
+	getline(temp, firstline);
+	while(temp >>  Op_name >> mincycle >> maxcycle){
+		Op_names.push_back(Op_name);
+       		mincycles.push_back(mincycle);
+ 		maxcycles.push_back(maxcycle);
 	}
+	cout << Op_names[0]<< endl;	
+
 
 }
 
