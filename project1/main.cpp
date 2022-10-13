f0222cc5f7a5d5907cccbc2152ba2be02617e86e
Vardan Tuladhar <tuladharvr@vcu.edu>
2022-10-12 20:40:36 -0400

    Removed OS_sim.cpp and OS_sim.h realized they were unneccassary (bad spelling). Just in case though it in a seprate branch. Added the basic UI to the code asking about the number of processes and the templates

--- project1/main.cpp
+++ project1/main.cpp
@@ -1,12 +1,41 @@
 #include <iostream>
-#include "OS_sim.h"
+//#include "OS_sim.h"
 #include <fstream>
+#include <vector>
 using namespace std;
 
 int main()
 {
-	string template1 = "Template.txt";
-	OS_sim sim1( template1);
+	bool debugging = true; 
+	int process_count, Template_count;
+	cout << "Hello and Welcome Vardan's OS simulator!!!" << endl;
+	cout << "How many processes would you like to create:";
+	cin >> process_count;
+	if (debugging == true)
+	{
+		cout << process_count<< endl;
+	}
+	cout << "How many templates are you loading in:";
+	cin >> Template_count;
+	if (debugging == true)
+	{
+		cout << Template_count << endl;
+	}
+	vector <string> templates;
+	string template1;
+	for (int i = 0; i < Template_count; i++)
+	{
+		cout << "Please enter the txt files for the template"<< endl;
+		cin >> template1;
+		templates.push_back(template1);
+		if (debugging == true)
+		{
+			cout << templates[i] << endl;
+		}
+
+	
+	}
+//	OS_sim sim1( template1);
 	return 0;
 
 }
