fe87bb7ebc8120f7d7f36f945798c32c612b70a9
Vardan Tuladhar <tuladharvr@vcu.edu>
2022-10-21 19:15:21 -0400

    added the function and state

---
+++ project1/functions.cpp
@@ -0,0 +1,5 @@
+#ifndef FUNCTION_H_INCLUDED
+#define FUNCTION_H_INCLUDED
+void decrement_runs( state rs);
+void check_waitingqueue(state rs, state ws);
+#endif 
---
+++ project1/state.h
@@ -0,0 +1,15 @@
+#ifndef STATE_H
+#define STATE_H
+class state{
+	public:
+		vector <process> processes_in;
+		string state;
+		int max_size ;
+		int total_cycle;
+		state(int set_max_size, string set_state);
+
+
+
+
+}
+#endif
